6aedc9c19d3f92ce7d8cf88168be10a3569c3c50
solimanhub <david.soliman2010@gmail.com>
2020-11-11 11:14:09 -0600

    agregado nuevo menu y conf de tema sddm

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -21,11 +21,13 @@ systemctl enable sddm.service
 #Creacion de carpetas en el Home/os2 y lanzador para instalar calamares
 xdg-user-dirs-update
 
-if [ -d /home/os2/Desktop ];
+mv /opt/sddm.conf /etc/
+
+if [ -d /home/os2/Desktop/ ];
 	then
 		cp /usr/share/applications/calamares.desktop /home/os2/Desktop/
 	else
-		if [ -d /home/os2/Escritorio ];
+		if [ -d /home/os2/Escritorio/ ];
 			then
 				cp /usr/share/applications/calamares.desktop /home/os2/Escritorio/
 			else
